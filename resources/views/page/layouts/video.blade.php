5af5de1147ce1d3eab3cc0c62662964050ee86b5
hatemfaysil <hatemfasyil16@gmail.com>
2021-11-02 17:12:27 +0200

    change footer

--- resources/views/page/layouts/video.blade.php
+++ resources/views/page/layouts/video.blade.php
@@ -10,7 +10,7 @@
       <div class="card" style="width: 22rem;margin:auto">
         <iframe src="https://www.youtube.com/embed/P8PfcdIeP3E" class="card-img-top" title="YouTube video" allowfullscreen frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <div class="card-body">
-        <h5 class="card-title">Light House ERB</h5>
+        <h5 class="card-title">Lighthouse ERP</h5>
         <p class="card-text">{{__('company.video_paragraph')}}</p>
         <a target="__blank" href="https://www.youtube.com/channel/UCs1TJvJ9xg5PwrgTRikJV_w"class="btn btn-primary"><strong>YouTube</strong></a>
         </div>
