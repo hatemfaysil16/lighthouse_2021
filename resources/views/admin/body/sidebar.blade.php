388c31f7b07b0dc0c1a2394f893117de40124a10
hatemfaysil <hatemfasyil16@gmail.com>
2021-11-04 17:06:43 +0200

    change sidebar for dashboard

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -56,10 +56,16 @@
       </li> 
 
       <li  class="" >
-      <a class="sidenav-item-link" href="{{ route('AllAbout') }}">
-      <span class="nav-text">Home About2</span>
+        <a class="sidenav-item-link" href="{{ route('service_two') }}">
+        <span class="nav-text">service_two</span>
+        </a>
+        </li> 
+
+      <li  class="" >
+      <a class="sidenav-item-link" href="{{ route('block') }}">
+      <span class="nav-text">Block Brand</span>
       </a>
-      </li> 
+      </li>  
 
 
       <li  class="" >
@@ -69,18 +75,83 @@
       </a>
       </li> 
 
-      <li  class="" >
-      <a class="sidenav-item-link" href="{{ route('all.brand') }}">
-      <span class="nav-text">Home Brand</span>
 
+    <li  class="" >
+    <a class="sidenav-item-link" href="{{ route('video') }}">
+    <span class="nav-text">video</span>
+    </a>
+    </li>            
+        
+    <li  class="" >
+    <a class="sidenav-item-link" href="{{ route('all.brand') }}">
+    <span class="nav-text">Home Brand</span>
+    </a>
+    </li>  
+  
+
+                  
+  </div>
+  </ul>
+</li>
+        
+
+
+
+                
+<li  class="has-sub" >
+      <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#ui-About"
+      aria-expanded="false" aria-controls="ui-About">
+      <i class="mdi mdi-folder-multiple-outline"></i>
+      <span class="nav-text"> HomeAbout</span> <b class="caret"></b>
       </a>
-      </li>  
+      <ul  class="collapse"  id="ui-About"
+      data-parent="#sidebar-menu">
+    <div class="sub-menu">
+    
+ 
+
+
+    <li  class="" >
+      <a class="sidenav-item-link" href="{{ route('home.about') }}">
+      <span class="nav-text">Home About</span>
+      </a>
+      </li> 
 
       <li  class="" >
-      <a class="sidenav-item-link" href="{{ route('block') }}">
-      <span class="nav-text">Block Brand</span>
+      <a class="sidenav-item-link" href="{{ route('team') }}">
+      <span class="nav-text">team</span>
       </a>
-      </li>  
+      </li> 
+
+
+      <li  class="" >
+      <a class="sidenav-item-link" href="{{ route('AllAbout') }}">
+      <span class="nav-text">Home About2</span>
+      </a>
+      </li> 
+
+
+
+
+          
+          
+                  
+  </div>
+  </ul>
+</li>
+
+
+            
+<li  class="has-sub" >
+    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#ui-services"
+    aria-expanded="false" aria-controls="ui-services">
+    <i class="mdi mdi-folder-multiple-outline"></i>
+    <span class="nav-text"> services</span> <b class="caret"></b>
+    </a>
+    <ul  class="collapse"  id="ui-services"
+    data-parent="#sidebar-menu">
+  <div class="sub-menu">
+
 
       <li  class="" >
       <a class="sidenav-item-link" href="{{ route('servicesAll') }}">
@@ -89,8 +160,8 @@
       </li>
       
       <li  class="" >
-      <a class="sidenav-item-link" href="{{ route('service_two') }}">
-      <span class="nav-text">service_two</span>
+      <a class="sidenav-item-link" href="{{ route('feature') }}">
+      <span class="nav-text">feature</span>
       </a>
       </li> 
 
@@ -100,33 +171,8 @@
       </a>
       </li>  
 
-      <li  class="" >
-        <a class="sidenav-item-link" href="{{ route('pricingAll') }}">
-        <span class="nav-text">pricing</span>
-        </a>
-        </li>  
 
 
-      <li  class="" >
-        <a class="sidenav-item-link" href="{{ route('video') }}">
-        <span class="nav-text">video</span>
-        </a>
-        </li>  
-
-      <li  class="" >
-        <a class="sidenav-item-link" href="{{ route('feature') }}">
-        <span class="nav-text">feature</span>
-        </a>
-        </li> 
-        
-      <li  class="" >
-        <a class="sidenav-item-link" href="{{ route('team') }}">
-        <span class="nav-text">team</span>
-        </a>
-        </li> 
-          
-          
-          
                   
   </div>
   </ul>
@@ -134,90 +180,147 @@
         
 
 
+<li  class="has-sub" >
+    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#ui-portfolio"
+    aria-expanded="false" aria-controls="ui-portfolio">
+    <i class="mdi mdi-folder-multiple-outline"></i>
+    <span class="nav-text"> portfolio</span> <b class="caret"></b>
+    </a>
+    <ul  class="collapse"  id="ui-portfolio"
+    data-parent="#sidebar-menu">
+  <div class="sub-menu">
 
-        
+    <li  class="" >
+      <a class="sidenav-item-link" href="{{ route('multi.image') }}">
+      <span class="nav-text">multi image</span>
+      </a>
+      </li> 
 
-        
-          <li  class="has-sub" >
-            <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#ui-elements"
-              aria-expanded="false" aria-controls="ui-elements">
-              <i class="mdi mdi-folder-multiple-outline"></i>
-              <span class="nav-text">Contact Page</span> <b class="caret"></b>
-            </a>
-            <ul  class="collapse"  id="ui-elements"
-              data-parent="#sidebar-menu">
-              <div class="sub-menu">
-                
-              <li  class="active" >
-<a class="sidenav-item-link" href="{{ route('admin.contact') }}">
-<span class="nav-text">Contact Profile</span>
-
-</a>
-</li> 
-<li  class="active" >
-<a class="sidenav-item-link" href="{{ route('admin.message') }}">
-<span class="nav-text">Contact Message</span>
-
-</a>
-</li> 
-
-
-<li  class="active" >
-  <a class="sidenav-item-link" href="{{ route('admin.SocialMedia') }}">
-  <span class="nav-text"> social media</span>
-  </a>
-  </li> 
-
-
-<li  class="active" >
-  <a class="sidenav-item-link" href="{{ route('info') }}">
-  <span class="nav-text">info</span>
-  </a>
-  </li> 
-
-
-  
-    
-                 
-                
-
-                
-              </div>
-            </ul>
-          </li>
-        
-
-        
-
-        
-          <li  class="has-sub" >
-            <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#charts"
-              aria-expanded="false" aria-controls="charts">
-              <i class="mdi mdi-chart-pie"></i>
-              <span class="nav-text">users</span> <b class="caret"></b>
-            </a>
-            <ul  class="collapse"  id="charts"
-              data-parent="#sidebar-menu">
-              <div class="sub-menu">
-                
-                
-                   
-                <li  class="active" >
-                  <a class="sidenav-item-link" href="{{ url('/' . $page='users') }}">
-                  <span class="nav-text"> List user</span>                
-                  </a>
-
-                  <a class="sidenav-item-link" href="{{ url('/' . $page='roles') }}">
-                    <span class="nav-text">Role users </span>                
-                    </a>
-                  </li> 
+      <li  class="" >
+        <a class="sidenav-item-link" href="{{ route('video') }}">
+        <span class="nav-text">video</span>
+        </a>
+        </li>   
+          
+          
                   
+  </div>
+  </ul>
+</li>
+
+
+        
+
+
+<li  class="has-sub" >
+    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#ui-pricing"
+    aria-expanded="false" aria-controls="ui-pricing">
+    <i class="mdi mdi-folder-multiple-outline"></i>
+    <span class="nav-text"> pricing</span> <b class="caret"></b>
+    </a>
+    <ul  class="collapse"  id="ui-pricing"
+    data-parent="#sidebar-menu">
+  <div class="sub-menu">
                 
 
+    <li  class="" >
+      <a class="sidenav-item-link" href="{{ route('pricingAll') }}">
+      <span class="nav-text">pricing</span>
+      </a>
+      </li>  
+
+
+
+
+          
+          
+                  
+  </div>
+  </ul>
+</li>
+
+
+
+
+
+
+
+
+
+        
+  <li  class="has-sub" >
+      <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#ui-elements"
+      aria-expanded="false" aria-controls="ui-elements">
+      <i class="mdi mdi-folder-multiple-outline"></i>
+      <span class="nav-text">Contact Page</span> <b class="caret"></b>
+      </a>
+      <ul  class="collapse"  id="ui-elements"
+      data-parent="#sidebar-menu">
+    <div class="sub-menu">
                 
-              </div>
-            </ul>
-          </li>
+      <li  class="active" >
+      <a class="sidenav-item-link" href="{{ route('admin.contact') }}">
+      <span class="nav-text">Contact Profile</span>
+
+      </a>
+      </li> 
+      <li  class="active" >
+      <a class="sidenav-item-link" href="{{ route('admin.message') }}">
+      <span class="nav-text">Contact Message</span>
+
+      </a>
+      </li> 
+
+
+      <li  class="active" >
+      <a class="sidenav-item-link" href="{{ route('admin.SocialMedia') }}">
+      <span class="nav-text"> social media</span>
+      </a>
+      </li> 
+
+
+      <li  class="active" >
+      <a class="sidenav-item-link" href="{{ route('info') }}">
+      <span class="nav-text">info</span>
+      </a>
+      </li> 
+
+    </div>
+    </ul>
+  </li>
+
+
+        
+
+        
+  <li  class="has-sub" >
+    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#charts"
+      aria-expanded="false" aria-controls="charts">
+      <i class="mdi mdi-chart-pie"></i>
+      <span class="nav-text">users</span> <b class="caret"></b>
+    </a>
+    <ul  class="collapse"  id="charts"
+      data-parent="#sidebar-menu">
+      <div class="sub-menu">
+        
+        
+            
+        <li  class="active" >
+          <a class="sidenav-item-link" href="{{ url('/' . $page='users') }}">
+          <span class="nav-text"> List user</span>                
+          </a>
+
+          <a class="sidenav-item-link" href="{{ url('/' . $page='roles') }}">
+            <span class="nav-text">Role users </span>                
+            </a>
+          </li> 
+          
+        
+
+        
+      </div>
+    </ul>
+  </li>
 
           
         
